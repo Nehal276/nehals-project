3bd8d0149911151eb5412fa02de2402938da0a80
Nehal Parab <nehal.n.parab@gmail.com>
2024-11-01 06:25:09 +0000

    Rename image files to remove spaces

--- src/Pages/PassionImage.tsx
+++ src/Pages/PassionImage.tsx
@@ -5,7 +5,7 @@ function PassionImage() {
         <div className="blog-page-container">
             <div className="bh-header">Passion Image</div>
             <div className="bh-content">
-                <img className='bh-headerImg' src='./passion image_3.png' alt="Header" />
+                <img className='bh-headerImg' src='./passionimage_3.png' alt="Header" />
                 <p className="bh-description">
                     <h4> Date- 11th October,2024</h4>
                     <div> 
--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -7,27 +7,27 @@ function Project() {
        Projects
       </header>
       <div className='posts'>
-                <img className="project-image" src='./Frame 65.png' alt="Nebuliser" />
+                <img className="project-image" src='./Frame65.png' alt="Nebuliser" />
             </div>
         <p className='project-text'>To redesign a nebulizer by using design cues from Nintendo, a gaming company. Nintendo is known for making people smile, 
           so incorporating that trait into a medical equipment like a nebulizer makes it more user-friendly and children may feel like 
           utilizing it rather than being compelled to do so.  </p>
       
           <div className='posts'>
-                <img className="project-image2" src='./Frame 66.png' alt="Vital Monitor" />
+                <img className="project-image2" src='./Frame66.png' alt="Vital Monitor" />
             </div>
         <p className='project-text'>To design a home vital monitor for Blood pressure, blood glucose, blood cholesterol and albumin for 
           early detection of gestational diabetes and preeclampsia during and after pregnancy for pregnant women with comorbidities like 
           obesity, diabetes, hypertension or history of diabetes/hypertension.  </p>
           
           <div className='posts'>
-                <img className="project-image2" src='./Frame 67.png' alt="Packaging" />
+                <img className="project-image2" src='./Frame67.png' alt="Packaging" />
             </div>
         <p className='project-text'>To design a packaging to store and transport bandages to provide improved 
           hygiene and easier application.  </p>
           
           <div className='posts'>
-                <img className="project-image2" src='./Frame 110.png' alt="App" />
+                <img className="project-image2" src='./Frame110.png' alt="App" />
             </div>
         <p className='project-text'>Connects the patient to the doctor and makes regular consultations with the doctor more convenient,
            while also giving small advices regarding healthcare  </p>
