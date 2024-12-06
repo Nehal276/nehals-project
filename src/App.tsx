08851613ab58cc19c233e176b1ecdccb31a808b3
Nehal Parab <nehal.n.parab@gmail.com>
2024-12-06 00:38:54 +0000

    phase2 update1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import './index.css';
 import './App.css';
 import { Provider } from 'react-redux';
--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import "./Resume.css";
-import Button from 'react-bootstrap/Button';
+
 
 function Resume() {
   return (
